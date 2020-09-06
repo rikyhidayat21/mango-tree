ac3435530bc35485ec59636d7fe970fdaac99b4f
rikyhidayat21 <rikyhidayat21@gmail.com>
2020-09-06 23:26:21 +0700

    update heigth stop growth if age = 13

--- index.js
+++ index.js
@@ -5,15 +5,16 @@
 class MangoTree {
 
   // Initialize a new MangoTree
-  constructor (healthStatus = true, harvested, maxAge = 17, maxHeight = 17) {
+  constructor (healthStatus = true, harvested, maxAge = 17, maxHeight = 17, stopProduction = 16, matureAge = 7) {
     this._age = 0
-    this._heigth = 0
+    this._height = 0
     this._fruits = []
     this._healthStatus = healthStatus
     this._harvested = harvested
     this._maxAge = maxAge
     this._maxHeight = maxHeight
-
+    this._stopProduction = stopProduction
+    this._matureAge = matureAge
   }
 
   get age () {
@@ -24,7 +25,10 @@ class MangoTree {
   }
 
   get height () {
-    return this._height;
+    return Number(this._height.toFixed(2))
+  }
+  get maxHeight() {
+    return this._maxHeight;
   }
 
   get fruits () {
@@ -46,14 +50,32 @@ class MangoTree {
 
   // Grow the tree
   grow () {
+    // nambah umur
     this._age++
     if (this._age === this._maxAge) {
-      this.healthStatus = false
+      this._healthStatus = false
+    }
+
+    if (this._age <= 13) {
+      let growUp = Math.random()
+      this._height += growUp
+      if (this._height >= this._maxHeight) {
+        this._height = this._maxHeight
+      } 
+    } else if (this._age > 13 && this._age <= this._maxAge) {
+      this._healthStatus = true
+    } else {
+      this._healthStatus = false
     }
   }
 
   // Produce some mangoes
   produceMangoes () {
+    
+    // menentukan umur untuk pohon berhenti bertambah tinggi
+    if (this._age >= 13 && this._age === this._stopProduction) {
+      
+    }
   }
 
   // Get some fruits
@@ -84,7 +106,7 @@ class Mango {
   let mangoTree = new MangoTree()
 do {
   mangoTree.grow();
-  // mangoTree.produceMangoes();
+  mangoTree.produceMangoes();
   // mangoTree.harvest();
   console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.height} | Fruits harvested = ${mangoTree.harvested}`)
 } while (mangoTree.healthStatus != false)
